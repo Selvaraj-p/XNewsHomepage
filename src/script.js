b9476613496ef8590e92b0974cd33275b35d9997
P Selvaraj <selvaraj.p88@gmail.com>
2025-07-04 19:42:29 +0530

    changes added

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
-const menuButton = document.getElementById("menu-button");
-const menuClose = document.getElementById("menu-close");
-const nav = document.querySelector("nav");
+const menu = document.querySelector("nav");
+const openBtn = document.getElementById("menu-button");
+const closeBtn = document.getElementById("menu-close");
 
-menuButton.addEventListener("click", () => {
-  nav.classList.add("open");
+openBtn.addEventListener("click", () => {
+  menu.classList.add("open");
 });
 
-menuClose.addEventListener("click", () => {
-  nav.classList.remove("open");
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
+
+window.addEventListener("load", () => {
+  const heroImg = document.getElementById("hero-image");
+
+  if (window.innerWidth < 1000) {
+    heroImg.src = "assets/image-web-3-mobile.jpg";
+  } else {
+    heroImg.src = "assets/image-web-3-desktop.jpg";
+  }
 });
